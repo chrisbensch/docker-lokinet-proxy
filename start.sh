3086372e9d072f98d5503eefd16b12dc9ada084c
Chris Bensch <chris.bensch@gmail.com>
2022-03-02 19:43:14 +0000

    Lokinet working proxy

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo 'nameserver 127.3.2.1' > /etc/resolv.conf
 
-screen -dmS lokinet /lokinet/build/daemon/lokinet
-screen -dmS fedproxy /usr/local/bin/fedproxy socks 127.0.0.1:2000 127.0.0.1:9050 127.0.0.1:4447
+/usr/local/bin/fedproxy socks 0.0.0.0:2000 0.0.0.0:9050 0.0.0.0:4447 &
+/lokinet/build/daemon/lokinet
